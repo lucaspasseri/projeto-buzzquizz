eb687109ce442b8404eeff1b718e4430d066a2e8
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-21 16:06:25 -0300

    Implementamos o carregamento e exibiçao básica de 'todos-os-quizzes' tela 1.

---
+++ js/scripts-primeirapag.js
@@ -0,0 +1,26 @@
+
+carregarQuizzes();
+
+function carregarQuizzes(){
+    const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes');
+    promessa.then(processarResposta);
+}
+
+function processarResposta(resposta) {
+    const seletorTodosOsQuizzes = document.querySelector(".todos-os-quizzes");
+    seletorTodosOsQuizzes.innerHTML ="";
+
+    for(let i = 0; i< resposta.data.length; i++){
+        seletorTodosOsQuizzes.innerHTML += `
+            <li onclick='abrirQuizzUnico(${resposta.data[i].id})' class="cartao-quizz">
+                <span>${resposta.data[i].title}</span>
+            </li>`;
+        let seletorUltimaLI = seletorTodosOsQuizzes.querySelector("li:last-of-type");
+        console.log(seletorUltimaLI, resposta.data[i].image);
+        seletorUltimaLI.style.backgroundImage = `url('${resposta.data[i].image}')`;
+    }
+}
+
+function abrirQuizzUnico(id){
+    alert(id);
+}
